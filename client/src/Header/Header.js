94ca2d41ecaec16d65fdfc512bf893d4de287ff0
Paulo Victor Campelo Selano <pv_sf@hotmail.com>
2018-10-22 21:44:09 -0300

    starter configuration to new project

--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -3,12 +3,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => (
   <div className="header">
-    <div className="header__title">My Reads</div>
-    {window.location.pathname === "/search" ? (
-      <Link to="/" className="header__home-button" />
-    ) : (
-      <Link to="/search" className="header__search-button" />
-    )}
+    <div className="header__title">Lecture</div>
   </div>
 );
 
